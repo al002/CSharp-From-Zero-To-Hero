7816aa1c23df4834933fb9decd2e0e65ec1ae461
al002 <al002@disroot.org>
2020-07-08 16:22:39 +0800

    Homework 1And2

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,25 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Your surname:");
+            var surname = Console.ReadLine();
+
+            Console.WriteLine("Your age:");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Your weight(kg):");
+            var weight = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your height(cm):");
+            var height = float.Parse(Console.ReadLine());
+
+            var heightInMeter = height / 100;
+            var bmi = weight / (heightInMeter * heightInMeter);
+
+            Console.WriteLine($"BMI: {bmi:N1}");
         }
     }
 }
